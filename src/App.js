765c66d72cd2eb46f54393d682bac575bd76fbdf
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2023-01-13 13:53:21 -0700

    restructured files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from "react";
 import "./App.css";
-import About from "./Screens/About";
-import Header from "./components/Header";
-import Contact from "./Screens/Contact";
+import Header from "./components/Header/Header";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Resume from "./Pages/Resume/Resume";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Contact />
-    </div>
+    <Router>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<About />} />
+
+            <Route path="/portfolio" element={<Portfolio />} />
+
+            <Route path="/contact" element={<Contact />} />
+
+            <Route path="/resume" element={<Resume />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/Screens/Contact.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from "react";
-import UserInput from "../components/user-input/UserInput";
-
-export default function Contact() {
-  return <UserInput />;
-}
