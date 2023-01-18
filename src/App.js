5bbc597513adb7dd0a2e0eb01ec1c70f9ab5cff0
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2023-01-18 13:21:14 -0700

    undid all attempted fixes

--- src/App.js
+++ src/App.js
@@ -6,41 +6,23 @@ import Contact from "./pages/contact/Contact.js";
 import Portfolio from "./pages/portfolio/Portfolio.js";
 import Resume from "./pages/resume/Resume.js";
 import Footer from "./components/Footer/Footer.js";
-
-// import { useState } from "react";
-
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//         <Header />
-//         <Routes>
-//           <Route path="/" element={<About />} />
-//           <Route path="/Portfolio" element={<Portfolio />} />
-//           <Route path="/Contact" element={<Contact />} />
-//           <Route path="/Resume" element={<Resume />} />
-//         </Routes>
-//         <Footer />
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <div>
-      <Header />
-      <About />
-      <Portfolio />
-      <Contact />
-      <Resume />
-      <Footer />
-    </div>
+    <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Resume" element={<Resume />} />
+        </Routes>
+        <Footer />
+    </BrowserRouter>
   );
 }
 
 export default App;
+
