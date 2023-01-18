77ecba377c691cb3a704a7650df169bc0cc25aeb
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2023-01-18 14:40:42 -0700

    readme

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -9,6 +9,9 @@ export default function Resume() {
         <p>
         AJAX, Bootstrap, Bulma, CSS, Express, Git, GitHub, Handlebars, HTML, JavaScript, JQuery, JSON, MongoDB, Mongoose, MySQL, Node.js, React, Sequelize
         </p>
+        <p>
+          Verbal and Written Communication, Creative Problem Solving, Management/Leadership, Scrum and Agile Methodologies, 
+        </p>
       </div>
       <div>
         <a className="download-btn" href="../mvResume.pdf" download>
